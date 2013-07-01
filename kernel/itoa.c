8ecf05331879a6c04473bdb5370607cdad59ad09
teslaNova <terranova@fahr-zur-hoelle.org>
2013-07-01 21:42:13 +0200

    ___moddi3, ___divdi3 fix

--- kernel/itoa.c
+++ kernel/itoa.c
@@ -4,6 +4,29 @@
 
 static const char digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWYXZ";
 
+u32 __divdi3(u64 v, u32 d) {
+  u32 vl = v & 0xFFFFFFFF, vh = v >> 31;
+  
+  __asm__ (
+    "div %%ebx"
+      : "=d"(vh), "=a"(vl) : "b"(d)
+  );
+  
+  return vl;
+} 
+
+u32 __moddi3(u64 v, u32 d) {
+  u32 vl = v & 0xFFFFFFFF, vh = v >> 31;
+  
+  __asm__ (
+    "div %%ebx"
+      : "=d"(vh), "=a"(vl) : "b"(d)
+  );
+  
+  return vh;
+  
+} 
+
 char *itoa(i32 num, char *buf, u32 base)
 {
   return ltoa(num, buf, base);
