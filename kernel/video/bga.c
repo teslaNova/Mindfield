3dbe652d502d06a808e09430852f4eb9a5420433
teslaNova <terranova@fahr-zur-hoelle.org>
2013-07-02 15:21:27 +0200

    font testing

--- kernel/video/bga.c
+++ kernel/video/bga.c
@@ -1,14 +1,18 @@
 #include <video/bga.h>
 
+#include <pci.h>
 #include <utils.h>
 
 #ifndef RES_X
-#define RES_X 800
+#define RES_X 640
 #endif
 
 #ifndef RES_Y
-#define RES_Y 600
+#define RES_Y 480
 #endif
+
+#define BGA_VENDOR 0x1234
+#define BGA_DEVICE 0x1111
 
 #define BGA_P_REGSEL 0x01CE
 #define BGA_P_REGDAT 0x01CF
@@ -22,8 +26,6 @@ enum {
   BGAC_BUILTIN_CAPS = 32,
 };
 
-u8 bga_capabilities = 0;
-
 enum bga_register {
   BGAR_ID = 0,
   BGAR_XRES,
@@ -31,7 +33,7 @@ enum bga_register {
   BGAR_BPP,
   BGAR_ENABLE,
   BGAR_BANK,
-  BGAR_VWITH,
+  BGAR_VWIDTH,
   BGAR_VHEIGHT,
   BGAR_XOFF,
   BGAR_YOFF,
@@ -40,8 +42,22 @@ enum bga_register {
 enum {
   BGA_EF_DISABLE = 0,
   BGA_EF_ENABLE = 1,
+  BGA_EF_LFR = 0x40,
   BGA_EF_NOT_CLEAR = 0x80
 };
+
+static u8 bga_capabilities = 0;
+
+static u32 *bga_video_ptr = (u32 *) 0xA0000;
+static u32 bga_video_pos_x = 0;
+static u32 bga_video_pos_y = 0;
+static u32 bga_font_px = 10;
+
+extern const u8 *bga_default_font_bitmap;
+extern const u8 bga_default_font_size_x;
+extern const u8 bga_default_font_size_y;
+
+static void bga_draw_32bpp(u32 x, u32 y, u32 c);
 
 static u16 bga_read(enum bga_register reg) {
   outw(BGA_P_REGSEL, reg);
@@ -54,7 +70,7 @@ static void bga_write(enum bga_register reg, u16 data) {
 }
 
 static void bga_set_state(bool enable) {
-  bga_write(BGAR_ENABLE, enable | BGA_EF_NOT_CLEAR);
+  bga_write(BGAR_ENABLE, enable | BGA_EF_NOT_CLEAR | BGA_EF_LFR);
 }
 
 bool bga_is_present(void) {
@@ -81,6 +97,14 @@ bool bga_init(void) {
   
   if(bga_set_resolution(RES_X, RES_Y) == false) {
     return false;
+  }
+  
+  const pci_entry_t *bga_dev = pci_get_device(BGA_VENDOR, BGA_DEVICE);
+
+  if(bga_dev != NULL) { // LINEAR FRAMEBUFFER TAKE ME!
+    bga_video_ptr = (u32 *) (bga_dev->data.type0.bar0.raw & 0xFFFFFFF0);
+  } else {
+    return false; // banked mode not supported
   }
   
   return true;
@@ -167,15 +191,31 @@ void bga_set_bank(u16 bank) {
   bga_write(BGAR_BANK, bank);
 }
 
-// screen compability
-static u32 *bga_video_ptr = 0xA0000;
-
 void bga_clear(void) {
-  bga_write(BGAR_ENABLE, BGA_EF_DISABLE);
-  bga_write(BGAR_ENABLE, BGA_EF_ENABLE);
+  bga_write(BGAR_ENABLE, BGA_EF_DISABLE | BGA_EF_LFR);
+  bga_write(BGAR_ENABLE, BGA_EF_ENABLE | BGA_EF_LFR);
 }
 
 //void bga_font_set(u8 *data);
 void bga_font_putc(char c) { 
+  u32 i = 0;//c - 0x30;
+  
+  bga_video_pos_x = RES_X / 2;
+  bga_video_pos_y = RES_X * 5;
+  
+  for(u32 j=0; j<bga_default_font_size_y; j++) {
+    for(u32 k=0; k<bga_default_font_size_x; k++) {
+      for(u32 pt=0; pt<4; pt++) {
+        u32 off = (pt + (k * bga_default_font_size_x) + bga_video_pos_x) + (RES_X * j + bga_video_pos_y);
+      
+        bga_video_ptr[off] = (bga_default_font_bitmap[i + j] & (1 << k) ? 0xFFFFFFFF : 0);
+      }
+    }
+  }
+  
+  for(;;);
+}
+
+static void bga_draw_32bpp(u32 x, u32 y, u32 c) {
   
 }
