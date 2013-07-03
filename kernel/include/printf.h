3ac0dfe20bacbfc6f0a2a8cf0f1938ab2635c379
droptable <murdoc@raidrush.org>
2013-07-03 17:03:14 +0200

    k_sprintf macro fix

--- kernel/include/printf.h
+++ kernel/include/printf.h
@@ -4,7 +4,7 @@
 #include <types.h>
 
 #define k_printf(args...) (void) k_snprintf(NULL, 0, args)
-#define k_sprintf(buf, fmt, args...) k_snprintf(buf, 0, fmt, args)
+#define k_sprintf(buf, args...) k_snprintf(buf, 0, args)
 
 u32 k_snprintf(char*, u32, const char*, ...);
 
