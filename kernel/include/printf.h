a62eaf9bffdc093a545c8561ca8ba7feec3a5a85
droptable <murdoc@raidrush.org>
2013-07-03 17:01:40 +0200

    fix done

--- kernel/include/printf.h
+++ kernel/include/printf.h
@@ -3,9 +3,9 @@
 
 #include <types.h>
 
+#define k_printf(args...) (void) k_snprintf(NULL, 0, args)
 #define k_sprintf(buf, fmt, args...) k_snprintf(buf, 0, fmt, args)
 
-u32 k_printf(const char*, ...);
 u32 k_snprintf(char*, u32, const char*, ...);
 
 #endif
