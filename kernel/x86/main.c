16e153252d36bc7c8fe0655c8588bd37fc7041d7
teslaNova <terranova@fahr-zur-hoelle.org>
2013-07-03 12:37:54 +0200

    updates

--- kernel/x86/main.c
+++ kernel/x86/main.c
@@ -2,6 +2,8 @@
 #include <types.h> // iN, uN
 
 #include <screen.h>
+#include <video/bga.h>
+
 #include <printf.h>
 
 #include <mm/pmm.h>
@@ -11,7 +13,7 @@
 
 #include <utils.h>
 
-#include <video/bga.h>
+#include <pci.h>
 
 void panic(void) {
   k_printf("panic() called. halting.");
@@ -21,9 +23,11 @@ void panic(void) {
 }
 
 void k_main(multiboot_info_t *mb_info, u32 mb_magic) {
+  pci_scan();
+  
   k_scrn_init();
   
-  k_printf("Mindfield\n\n");
+  k_printf("Mindfield\n");
   
   if(mb_magic != MULTIBOOT_BOOTLOADER_MAGIC) {
     k_printf("System Halted.\n");
@@ -33,10 +37,8 @@ void k_main(multiboot_info_t *mb_info, u32 mb_magic) {
   k_printf("- Initializing PMM\n\n");
   pmm_setup(mb_info->mmap_addr, mb_info->mmap_length);
   
-  for(rtc_datetime_t dt={.year=-1};;rtc_get_datetime(&dt)) {
-    if(dt.year != (u16)-1) {
-      k_printf("It is %d.%d.%d %d:%d:%d.\n", dt.day, dt.month, dt.year, dt.hours, dt.minutes, dt.seconds);
-    }
+  for(rtc_datetime_t dt={0};;rtc_get_datetime(&dt)) {
+    k_printf("\rIt is %d.%d.%d %d:%d:%d", dt.day, dt.month, dt.year, dt.hours, dt.minutes, dt.seconds);
     rtc_sleep(1);
   }
 }
