ffe1abdd34e41759cc71eaa42c2b8dff940dbe6e
teslaNova <terranova@fahr-zur-hoelle.org>
2013-07-10 07:27:55 +0200

    null segment fix

--- kernel/x86/descriptor.c
+++ kernel/x86/descriptor.c
@@ -3,6 +3,9 @@
 #include <mm/sm.h>
 
 segment_t segment_tbl[] = {
+  // null desc
+  {0},
+  
   // cs ring 0
   {
     .pl = 0, 
@@ -54,23 +57,6 @@ segment_t segment_tbl[] = {
     .base_addr_high = 0xFF
   },
   
-  // ds ring 0
-  {
-    .pl = 0, 
-    .limit_low = 0x3FFF,
-    .base_addr_low = 0xFFFFFF,
-    .type.data.accessed = 1, .type.data.writeable = 1, .type.data.bottom_top = 0, .type.data._false = 0,
-    .cd = 1,
-    .dpl = 0,
-    .present = 1,
-    .limit_high = 0xF,
-    .avl = 1,
-    .x64 = 0,
-    .big = 1,
-    .granularity = 1,
-    .base_addr_high = 0xFF
-  },
-  
   // ds ring 3
   {
     .pl = 3, 
@@ -86,10 +72,7 @@ segment_t segment_tbl[] = {
     .big = 1,
     .granularity = 1,
     .base_addr_high = 0xFF
-  },
-  
-  // null desc
-  {0}
+  }
 };
 
 descriptor_t desc_tbl[DT_SIZE] = {
